103a46478eb726a1522e5688c567820d855adf31
Karter13 <KarMiH1975@mail.ru>
2020-07-20 13:19:08 +0300

    added change status in Checkbox

--- src/components/UniversalComponents/UniversalComponents.tsx
+++ src/components/UniversalComponents/UniversalComponents.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Checkbox} from '../common/Checkbox/Checkbox';
 import {Input} from '../common/Input/Input';
 import {Button} from '../common/Button/Button';
 
 export const UniversalComponents = () => {
+    let [checkboxValue, setCheckboxValue] = useState<boolean>(true);
+    const changeValue = (value: boolean) => {
+        setCheckboxValue(value);
+    };
+
     return (
         <div>
-            <Checkbox/>
+            <Checkbox checkboxValue={checkboxValue} changeValue={changeValue}/>
             <Input onChange={(value) => {console.log(value)}} value={'INPUT'}/>
             <Button value={'BUTTON'} click={() => console.log(('click'))}/>
         </div>
--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import style from './Button.module.css'
 
-export type ButtonProrsType = {
+export type ButtonPropsType = {
     click: () => void
     value: string
 }
 
-export const Button: React.FC<ButtonProrsType> = (props) => {
+export const Button: React.FC<ButtonPropsType> = (props) => {
     return (
         <>
             <button className={style.button} onClick={props.click}>{props.value}</button>
