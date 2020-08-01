0cdcaa74f6cb7f15ad9cca0222669371aaea57c5
Karter13 <KarMiH1975@mail.ru>
2020-08-01 11:54:38 +0300

    added functions to save state for localStorage

--- src/components/Junior/Junior.tsx
+++ src/components/Junior/Junior.tsx
@@ -1,10 +1,18 @@
 import React, {useState} from 'react';
 import {EditableSpan} from '../common/EditableSpan/EditableSpan';
 
+export type StateType = {
+    x: string
+    y: number
+}
+
 export const Junior = () => {
 
     let [value, setValue] = useState('EditableSpan');
     let [error, setError] = useState<string | null>(null);
+    let [state, setState] = useState<Array<StateType>>([
+        {x: value, y: 10}
+    ]);
 
     const onChange = (value: string) => {
         if (value.trim()) {
@@ -16,6 +24,29 @@ export const Junior = () => {
     };
 
 
+
+
+    function saveState<T>(key: string, state: T) {
+        const stateAsString = JSON.stringify(state);
+        localStorage.setItem(key, stateAsString)
+    }
+
+    function restoreState<T>(key: string, defaultState: T) {
+        const stateAsString = localStorage.getItem(key);
+        if (stateAsString !== null) defaultState = JSON.parse(stateAsString) as T;
+        return defaultState;
+    }
+
+    function getDataFromStore() {
+        const newState: StateType = restoreState<StateType>('test', {x: '', y: 0});
+        const newStates = [newState, ...state];
+        console.log(newStates);
+
+        setState(newStates);
+
+    }
+
+
     return (
         <div>
             <h3>Junior</h3>
@@ -23,8 +54,11 @@ export const Junior = () => {
                           onChange={onChange}
                           error={error}
             />
-            <button>X</button>
-            <button>X</button>
+            <button onClick={() => {
+                saveState<StateType>('test', {x: value, y: 2})
+            }}>Save</button>
+            <button onClick={() => {getDataFromStore()}}>Restore</button>
+            <span>{state[0].x}</span>
         </div>
     )
-};
+}
