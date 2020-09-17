6d5ab18cf8db37788754fe1cc785b80268b08627
Karter13 <KarMiH1975@mail.ru>
2020-09-17 17:37:14 +0300

    commit for test

--- src/components/JuniorPlus/JuniorPlus.tsx
+++ src/components/JuniorPlus/JuniorPlus.tsx
@@ -2,12 +2,18 @@ import React, {useCallback, useState} from 'react';
 import style from './JuniorPlus.module.css'
 import {Radio} from '../common/Radio/Radio';
 import {RadioType} from '../Junior/Junior';
+import {connect} from 'react-redux';
+import {GlobalStateType} from '../../redux/redux-store';
+import {setTheme} from '../../redux/themeJuniorPlus-reducer';
 
 export const JuniorPlus = () => {
     const [radioValue, setRadioValue] = useState<Array<RadioType>>([
-        {id: 1, name: 'Light', checked: false},
-        {id: 2, name: 'Dark', checked: false},
+        {id: 1, name: 'start', checked: true},
+        {id: 2, name: 'Light', checked: false},
+        {id: 3, name: 'Dark', checked: false},
     ]);
+
+    // let classNameStyle = props.currentTheme.name === 'start' ? style.container1:  props.currentTheme.name === 'Light' ?  style.container2:  style.container3
 
     const changeRadio = useCallback((id: number, value: any | boolean) => {
         radioValue.map(r => {
@@ -22,10 +28,19 @@ export const JuniorPlus = () => {
 
 
     return (
-        <div className={ style.container}>
+        <div className={style.container}>
             <h1>JuniorPlus</h1>
             <Radio radioValue={radioValue} onChange={changeRadio}/>
         </div>
 
     )
 };
+
+
+const mapStateToProps = (store: GlobalStateType) => ({
+    theme: store.theme.theme
+});
+
+
+export default connect(mapStateToProps, {setTheme})(JuniorPlus);
+
--- src/redux/themeJuniorPlus-reducer.ts
+++ src/redux/themeJuniorPlus-reducer.ts
@@ -10,7 +10,6 @@ type Theme = {
 }
 // type ThemeJuniorPlusType = {
 //     light: Theme
-//     middle: Theme
 //     dark: Theme
 // }
 
